549447e7172d5623d129d91cfb2b6976d896d265
Кукса <kuiv1989@gmail.com>
2023-08-12 18:37:01 +0300

    add btnLoad behavior and start creating webview url itercept behavior

--- app/src/main/java/com/winl/ine/foot/w4c2rtesttask/MainActivity.kt
+++ app/src/main/java/com/winl/ine/foot/w4c2rtesttask/MainActivity.kt
@@ -2,10 +2,79 @@ package com.winl.ine.foot.w4c2rtesttask
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import com.winl.ine.foot.w4c2rtesttask.databinding.ActivityMainBinding
+
+private const val baseUrl = "http://app.zaimforyou.ru/hello"
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding:ActivityMainBinding?=null
+    private val binding:ActivityMainBinding
+        get() = _binding!!
+    private val webView by lazy { this.binding.mainWebView }
+    private val pbLoading by lazy { this.binding.pbLoading }
+    private val btnLoad by lazy { this.binding.btnLoad }
+    private val tvId by lazy { this.binding.tvID }
+    private val tvUuid by lazy { this.binding.tvUUID }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupBtnClickListener()
+    }
+
+    private fun setupBtnClickListener(){
+        btnLoad.setOnClickListener {
+                hideView(btnLoad)
+                showView(pbLoading)
+                loadPage()
+        }
+    }
+
+    private fun setupWebView(){
+        with(webView){
+            loadUrl(baseUrl)
+            webViewClient = webViewClient()
+        }
+    }
+
+    private class webViewClient():WebViewClient(){
+
+        override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+            Log.d("URL", url.toString())
+            return super.shouldOverrideUrlLoading(view, url)
+        }
+
+        override fun shouldOverrideUrlLoading(
+            view: WebView?,
+            request: WebResourceRequest?
+        ): Boolean {
+            Log.d("URL", request.toString())
+
+            return super.shouldOverrideUrlLoading(view, request)
+        }
+    }
+
+    private fun loadPage(){
+        setupWebView()
+    }
+
+    private fun hideView(view: View){
+        view.visibility = View.GONE
+    }
+
+    private fun showView(view: View){
+        view.visibility = View.VISIBLE
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
