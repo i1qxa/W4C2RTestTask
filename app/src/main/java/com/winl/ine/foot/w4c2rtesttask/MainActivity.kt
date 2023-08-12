d42617910d5ddf3149446c8070e418ee945c8da8
Кукса <kuiv1989@gmail.com>
2023-08-12 16:57:52 +0300

    Initial commit

---
+++ app/src/main/java/com/winl/ine/foot/w4c2rtesttask/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.winl.ine.foot.w4c2rtesttask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
